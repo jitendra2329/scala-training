45c3bdd49fe7d70393bfaae753832ff0a98e2aa7
Jitendra <jitendra.kumar@knoldus.com>
2023-04-20 15:26:50 +0530

    changes added

--- src/main/scala/com/knoldus/day2/TypeParameterization.scala
+++ src/main/scala/com/knoldus/day2/TypeParameterization.scala
@@ -1,14 +1,12 @@
 package com.knoldus.day2
 
-import scala.jdk.Accumulator
-
 object TypeParameterization extends App {
 
-  val listOfInt = new Lists[Int]
+  private val listOfInt = new Lists[Int]
   println(listOfInt.createList(List().empty, 4))
 
   val listOfStrings = new Lists[String]
-  println(listOfStrings.createList(List().empty, "Jitendra"))
+  println(listOfStrings.createList(List().empty,"Jitendra"))
 
 }
 
